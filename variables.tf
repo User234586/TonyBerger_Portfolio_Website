ba6de6f3d4b6fcc1ccf878a8a13b64e1b88627b8
Tony Berger <90921536+User234586@users.noreply.github.com>
2025-02-27 20:15:00 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,69 @@
+variable "AWS_REGION" {
+  description = "AWS region"
+  default     = "us-west-2"
+}
+
+variable "vpc_cidr_block" {
+  description = "VPC CIDR Block"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_1_cidr" {
+  description = "CIDR block for public subnet 1"
+  default     = "10.0.1.0/24"
+}
+
+variable "public_subnet_2_cidr" {
+  description = "CIDR block for public subnet 2"
+  default     = "10.0.2.0/24"
+}
+
+variable "private_subnet_1_cidr" {
+  description = "CIDR block for private subnet 1"
+  default     = "10.0.3.0/24"
+}
+
+variable "private_subnet_2_cidr" {
+  description = "CIDR block for private subnet 2"
+  default     = "10.0.4.0/24"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
+
+variable "db_username" {
+  description = "RDS Master Username"
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "RDS Master Password"
+  sensitive   = true
+}
+
+variable "my_ip" {
+  description = "Your public IP address"
+  type        = string
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS access key ID"
+  sensitive   = true
+}
+
+variable "AWS_SESSION_TOKEN" {
+  description = "AWS session token"
+  sensitive   = true
+}
+
+variable "amis" {
+  type        = map(string)
+  description = "Region specific AMI"
+  default = {
+    us-west-2    = "ami-000089c8d02060104"
+    eu-central-1 = "ami-0ec8c354f85e48227"
+  }
+
+}
